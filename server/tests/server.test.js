43aa373045a4d667fc12096f11df7c9e793959c7
Yokesh <byokeshb@gmail.com>
2018-11-25 19:33:57 +0530

    Add text for /todos

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -5,8 +5,17 @@ const {app}=require('./../server');
 const {Todo}=require('./../models/todo');
 
 
+const todos=[{
+  text:"First todo"
+},
+{
+  text:"Second todo"
+}];
+
 beforeEach((done)=>{
-  Todo.remove({}).then(()=>done());
+  Todo.remove({}).then(()=>{
+    return Todo.insertMany(todos);
+  }).then(()=>done());
 });
 
 
@@ -25,7 +34,7 @@ describe('POST /todos',()=>{
       if(err){
         return done(err);
       }
-      Todo.find().then((todos)=>{
+      Todo.find({text}).then((todos)=>{
         expect(todos.length).toBe(1);
         expect(todos[0].text).toBe(text);
         done();
@@ -46,7 +55,7 @@ describe('POST /todos',()=>{
         return done(err);
       }
       Todo.find().then((todos)=>{
-        expect(todos.length).toBe(0);
+        expect(todos.length).toBe(2);
         done();
       }).catch((e)=>{
         done(e);
@@ -54,4 +63,16 @@ describe('POST /todos',()=>{
     });
   });
 
+});
+describe('GET /todos',()=>{
+  it('should get all todos',(done)=>{
+    request(app)
+    .get('/todos')
+    .expect(200)
+    .expect((res)=>{
+      expect(todos.length).toBe(2);
+    })
+    .end(done);
+
+    });
 });
