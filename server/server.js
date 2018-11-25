209cb44c502cfb03d563d7959ccd94d0d8dd9fce
Yokesh <byokeshb@gmail.com>
2018-11-25 19:08:47 +0530

    Added second route get /todos

--- server/server.js
+++ server/server.js
@@ -10,6 +10,8 @@ var app=express();
 
 app.use(bodyParser.json());
 
+
+
 app.post('/todos',(req,res)=>{
   var todo=new Todo({
     text:req.body.text
@@ -19,6 +21,14 @@ app.post('/todos',(req,res)=>{
   },(e)=>{
     res.status(400).send(e);
   });
+});
+
+app.get('/todos',(req,res)=>{
+  Todo.find().then((todos)=>{
+    res.send({todos})
+  },(e)=>{
+    res.status(400).send(e);
+  })
 });
 
 app.listen(3000,()=>{
