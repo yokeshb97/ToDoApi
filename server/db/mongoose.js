b0ff1ed07bfed717f913cfb0e2632e0ec86d83cd
Yokesh <byokeshb@gmail.com>
2018-11-26 13:23:58 +0530

    Deploying to heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,10 @@
 var mongoose=require('mongoose');
 
 mongoose.Promise=global.Promise;
-mongoose.connect('mongodb://localhost:27017/Todoapp');
-
+//mongoose.connect('mongodb://localhost:27017/Todoapp');
+let db = {
+  localhost: 'mongodb://localhost:27017/TodoApp',
+  mlab: 'mongodb://yoks:asdf1234@ds063158.mlab.com:63158/todorest101'
+};
+mongoose.connect( process.env.PORT ? db.mlab : db.localhost);
 module.exports={mongoose};
