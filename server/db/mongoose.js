7d0cc1fc599b60a5e1dc2dfe346fd52e4a0dfa83
Yokesh <byokeshb@gmail.com>
2018-11-26 19:32:19 +0530

    Setup config

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,5 +3,5 @@ var mongoose=require('mongoose');
 mongoose.Promise=global.Promise;
 //mongoose.connect('mongodb://localhost:27017/Todoapp');
 
-mongoose.connect (process.env.MONGODB_URI || 'mongodb://localhost:27017/Todoapp');
+mongoose.connect (process.env.MONGODB_URI);
 module.exports={mongoose};
