92361cf42362fe049ca08f046cc4c7b3fcaa8ab7
Yokesh <byokeshb@gmail.com>
2018-11-26 14:26:03 +0530

    Changes heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,9 +2,6 @@ var mongoose=require('mongoose');
 
 mongoose.Promise=global.Promise;
 //mongoose.connect('mongodb://localhost:27017/Todoapp');
-let db = {
-  localhost: 'mongodb://localhost:27017/TodoApp',
-  mlab: 'mongodb://yoks:asdf1234@ds063158.mlab.com:63158/todorest101'
-};
-mongoose.connect( process.env.PORT ? db.mlab : db.localhost);
+
+mongoose.connect (process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
 module.exports={mongoose};
