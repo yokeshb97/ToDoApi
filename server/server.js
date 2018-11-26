e66d310873b46c3b1f267ed3ac7b5e625dfe5ceb
Yokesh <byokeshb@gmail.com>
2018-11-26 18:10:04 +0530

    Update todo

--- server/server.js
+++ server/server.js
@@ -1,7 +1,9 @@
-var express=require('express');
-var bodyParser=require('body-parser');
-
+const _=require('lodash');
+const express=require('express');
+const bodyParser=require('body-parser');
 const {ObjectID}=require('mongodb');
+
+
 var {mongoose}=require('./db/mongoose');
 var {Todo}=require('./models/todo');
 var {User}=require('./models/user');
@@ -62,6 +64,33 @@ app.delete('/todos/:id',(req,res)=>{
     res.status(400).send();
   });
 });
+
+app.patch('/todos/:id',(req,res)=>{
+  var id=req.params.id;
+  var body=_.pick(req.body,['text','completed']);
+  if(!ObjectID.isValid(id)){
+    return res.status(404).send();
+  }
+  if(_.isBoolean(body.completed)&&body.completed){
+    body.completedAt=new Date().getTime();
+  }else{
+      body.completed=false,
+      body.completedAt=null
+  }
+  Todo.findByIdAndUpdate(id,{$set:body},{new:true}).then((todo)=>{
+    if(todo)
+      res.status(200).send({todo});
+    else{
+      res.status(404).send();
+    }
+
+  }).catch((e)=>{
+    res.status(400).send();
+  });
+
+});
+
+
 
 app.listen(port,()=>{
   console.log('Started on',port);
