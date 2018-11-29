d39b177663e34253482bb9219e3bb42f65ae2a84
Yokesh <byokeshb@gmail.com>
2018-11-29 20:04:55 +0530

    Authenticate

--- server/models/user.js
+++ server/models/user.js
@@ -49,6 +49,24 @@ UserSchema.methods.generateAuthToken = function () {
     return token;
   });
 };
+
+UserSchema.statics.findByToken= function(token){
+  var User=this;
+  var decoded;
+  try{
+    decoded=jwt.verify(token,'abc234');
+  }
+  catch(e){
+    return Promise.reject();
+  }
+
+  return User.findOne({
+    _id:decoded._id,
+    'tokens.token':token,
+    'tokens.access':'auth'
+  });
+}
+
 var User=mongoose.model('User',UserSchema);
 
 module.exports={User};
--- server/server.js
+++ server/server.js
@@ -10,6 +10,7 @@ const {ObjectID}=require('mongodb');
 var {mongoose}=require('./db/mongoose');
 var {Todo}=require('./models/todo');
 var {User}=require('./models/user');
+var {authenticate}=require('./middleware/authenticate');
 
 var app=express();
 const port =process.env.PORT;
@@ -104,6 +105,12 @@ app.post('/users',(req,res)=>{
     res.status(400).send(e);
   });
 });
+
+
+app.get('/users/me',authenticate,(req,res)=>{
+  res.send(req.user);
+});
+
 
 app.listen(port,()=>{
   console.log('Started on',port);
