85aae35298b02e73abad2b8583f0db73a284f90f
Yokesh <byokeshb@gmail.com>
2018-11-30 16:39:26 +0530

    Test login

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -236,3 +236,44 @@ describe('POST /users',()=>{
     .end(done);
 });
 });
+describe('POST /users/login',()=>{
+
+  it('should login for valid credentials',()=>{
+    request(app)
+    .post('/users/login')
+    .send({
+      email:users[1].email,
+      password:users[1].password
+    })
+    .expect(200)
+    .expect((res)=>{
+      expect(res.headers['x-auth']).toExist();
+    })
+    .end((err,res)=>{
+      if(err){
+        return done(err);
+      }
+      User.findById(users[1]._id).then((user)=>{
+        expect(user.tokens[0]).toInclude({
+          access:'auth',
+          token:res.headers['x-auth']
+        });
+        done();
+      }).catch((e)=>done(e));
+    });
+  });
+/*  it('should not login for invalid credentials',()=>{
+    request(app)
+    .post('/users/login')
+    .send({
+      email:users[1].email,
+      password:'abc678'
+    })
+    .expect(400)
+    .expect((res)=>{
+      expect(res.headers['x-auth']).toNotExist();
+    })
+    .end(done);
+});*/
+
+});
